532e2c9387fe69a28f7b5d06e9306110bb86731c
zliu9 <zhanglin.liu@intel.com>
2015-05-09 21:59:18 +0800

    - fixed toggle stat - set initial value to 1

--- atlas_nodes/toggle/toggle.js
+++ atlas_nodes/toggle/toggle.js
@@ -11,28 +11,33 @@ module.exports = function(RED) {
         var stat = parseInt(config.initVal);
 
         function setStat(val) {
-        	if(!val) 
-                node.status({fill: 'green', shape:'dot', text: 'on'});
-            else
-            	node.status({fill: 'red', shape:'dot', text: 'off'});
+        if(val) 
+            node.status({fill: 'green', shape:'dot', text: 'on'});
+        else
+            node.status({fill: 'red', shape:'dot', text: 'off'});
         }
 
-        setStat(stat);
+        function sendStat() {
+            var val = stat ? 1 : 0;
+
+            node.log('send ' + val);
+            node.send({'payload': val});
+
+            setStat(stat);
+            stat = !stat;
+        }
 
         node.on('input', function(msg) {
 
             console.log('recv: ' + JSON.stringify(msg));
 
-            var val = stat ? 1 : 0;
-
-            node.log('send ' + val);
-        	node.send({'payload': val});
-
-        	stat = !stat;
-
-        	setStat(stat);
+            sendStat();
         });
-    }
+
+        node.log('send initial ' + stat);
+        node.send({'payload': stat});
+        setStat(stat); 
+   }
 
     RED.nodes.registerType('toggle', toggle);
 
