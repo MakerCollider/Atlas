a038ab8e68c743fc06459c477260421c8831ad2f
myloveed <myloveed@github.com>
2015-05-10 15:50:08 +0000

    rename install.sh to gene_release.sh; add -g option in env setup script

--- install.sh
+++
@@ -1,3 +0,0 @@
-cd ..
-TAR_NAME=atlas_`date +"%Y_%m_%d"`.tgz
-tar -czvf $TAR_NAME --exclude=flows_* --exclude=.git --exclude=.DS_Store node-red Atlas
--- npm_env_setup.sh
+++ npm_env_setup.sh
@@ -1,24 +1,27 @@
 #/bin/bash
+set -x
 cd ../node-red
-npm config set registry http://registry.cnpmjs.org
+npm config set registry http://registry.cnpmjs.org
+npm install -g node-gyp
 echo "configuration node-red ......"
 npm install
+npm install -g grunt-cli
+grunt build
 echo "installing bindings ......"
-npm install bindings
-echo "installing node-gyp ......"
-npm install node-gyp
+npm install -g bindings
 echo "installing debug ......"
-npm install debug
+npm install -g debug
 echo "installing express ......"
-npm install express
+npm install -g express
 echo "installing socket.io ......"
-npm install socket.io
+npm install -g socket.io
 echo "installing serve-favicon ......"
-npm install serve-favicon
+npm install -g serve-favicon
 echo "installing morgan ......"
-npm install morgan
+npm install -g morgan
 echo "installing cookie-parser ......"
-npm install cookie-parser
+npm install -g cookie-parser
 echo "installing body-parser ......"
-npm install body-parser
+npm install -g body-parser
 cd ../
+set +x
